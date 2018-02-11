bc0b848b39bb4c03b0412165e3fb9a0bc645df78
İren SALTALI <irensaltali@hotmail.com>
2018-02-11 13:57:14 +0300

    Init with default MVC

---
+++ AspNetCoreMVCLocalizationByURL/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreMVCLocalizationByURL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
